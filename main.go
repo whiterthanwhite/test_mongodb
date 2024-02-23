f6155cbf6326f770b0a7ba58618c7b95fd41466b
whiterthanwhite <0whiterthanwhite0@gmail.com>
2024-02-23 20:44:19 +0400

    add insert, find, delete functions

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func main() {
 	if mongodbURI == "" {
 		panic("Specify mongodb connection URI")
 	}
-	mongodbDBName := os.Getenv("MONGODB_DB")
-	if mongodbDBName == "" {
+	dbName := os.Getenv("MONGODB_DB")
+	if dbName == "" {
 		panic("Specify mongodb database connection!")
 	}
 
@@ -32,9 +32,125 @@ func main() {
 	}
 	defer client.Disconnect(ctx)
 
-	var result bson.M
-	if err = client.Database(mongodbDBName).RunCommand(ctx, bson.D{{"ping", 1}}).Decode(&result); err != nil {
+	if err = testDatabaseConnection(ctx, client, dbName); err != nil {
 		panic(err)
 	}
+
+	if err = testInsertOne(ctx, client, dbName, "persons"); err != nil {
+		panic(err)
+	}
+
+	if err = testInsertMany(ctx, client, dbName, "persons"); err != nil {
+		panic(err)
+	}
+
+	if err = testFindOne(ctx, client, dbName, "persons"); err != nil {
+		panic(err)
+	}
+
+	if err = testFind(ctx, client, dbName, "persons"); err != nil {
+		panic(err)
+	}
+
+	if err = testDeleteMany(ctx, client, dbName, "persons"); err != nil {
+		panic(err)
+	}
+}
+
+func testDatabaseConnection(ctx context.Context, client *mongo.Client, dbName string) error {
+	var result bson.M
+	if err := client.Database(dbName).RunCommand(ctx, bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		return err
+	}
 	fmt.Println("Succesful connection!")
+	return nil
+}
+
+type Person struct {
+	FirstName string `bson:"first_name,omitempty"`
+	LastName  string `bson:"last_name,omitempty"`
+}
+
+func testInsertOne(ctx context.Context, client *mongo.Client, dbName, collName string) error {
+	// if collection doesn't exist. it will be created automatically
+	coll := client.Database(dbName).Collection(collName)
+	p := &Person{
+		FirstName: "Test Person First Name",
+		LastName:  "Test Person Last Name",
+	}
+
+	_, err := coll.InsertOne(ctx, p)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Person", *p, "created successfully")
+	return nil
+}
+
+func testInsertMany(ctx context.Context, client *mongo.Client, dbName, collName string) error {
+	coll := client.Database(dbName).Collection(collName)
+	persons := []interface{}{
+		&Person{
+			FirstName: "Leonardo",
+			LastName:  "Turtle",
+		},
+		&Person{
+			FirstName: "Donatello",
+			LastName:  "Turtle",
+		},
+	}
+
+	_, err := coll.InsertMany(ctx, persons)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(len(persons), "documents created successfully")
+	return nil
+}
+
+func testFindOne(ctx context.Context, client *mongo.Client, dbName, collName string) error {
+	coll := client.Database(dbName).Collection(collName)
+	personFilter := bson.D{{"first_name", "Donatello"}}
+
+	var p Person
+	err := coll.FindOne(ctx, personFilter).Decode(&p)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Find", p)
+	return nil
+}
+
+func testFind(ctx context.Context, client *mongo.Client, dbName, collName string) error {
+	coll := client.Database(dbName).Collection(collName)
+	personFilter := bson.D{{"first_name", "Donatello"}}
+
+	cur, err := coll.Find(ctx, personFilter)
+	if err != nil {
+		return err
+	}
+
+	var persons []Person
+	if err = cur.All(ctx, &persons); err != nil {
+		return err
+	}
+
+	fmt.Println("Retreived", len(persons))
+	return nil
+}
+
+func testDeleteMany(ctx context.Context, client *mongo.Client, dbName, collName string) error {
+	coll := client.Database(dbName).Collection(collName)
+	filter := bson.D{{}}
+
+	res, err := coll.DeleteMany(ctx, filter)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Deleted", res.DeletedCount, "documents!")
+	return nil
 }
